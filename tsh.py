d4f2ee84ee2f7729ae8a1d07bbc31386f1361250
johnli0135 <johnli0135@gmail.com>
2018-04-07 00:22:47 -0400

    first

---
+++ tsh.py
@@ -0,0 +1,132 @@
+import imaplib
+import smtplib
+import email
+import time
+import sys
+import subprocess
+from dateparser.search import search_dates
+from getpass import getpass
+
+def emails(inbox, phoneaddress, earliest):
+    # adapted from https://codehandbook.org/how-to-read-email-from-gmail-using-python/
+    inbox.select("inbox")
+    type, data = inbox.search(None, "ALL")
+    ids = data[0].split()
+    result = []
+
+    for i in range(int(ids[-1]), int(ids[0]), -1):
+        type, data = inbox.fetch(str(i), "(RFC822)")
+
+        for part in data:
+            if isinstance(part, tuple):
+                msg = email.message_from_string(part[1].decode("utf-8"))
+
+                sender = msg["From"]
+                if sender != phoneaddress:
+                    break
+
+                try: # sometimes breaks with error about date comparisons
+                    dates = search_dates(msg["Date"])
+                except:
+                    continue
+                timestamp = dates[0][1].timestamp()
+
+                if timestamp < earliest:
+                    break
+
+                # adapted from https://stackoverflow.com/questions/17874360/python-how-to-parse-the-body-from-a-raw-email-given-that-raw-email-does-not
+                if msg.is_multipart():
+                    for part in msg.walk():
+                        ctype = part.get_content_type()
+                        cdispo = str(part.get("Content-Disposition"))
+
+                        # skip any text/plain (txt) attachments
+                        if ctype == "text/plain" and "attachment" not in cdispo:
+                            body = part.get_payload(decode=True)  # decode
+                            break
+                # not multipart - i.e. plain text, no attachments, keeping fingers crossed
+                else:
+                    body = msg.get_payload(decode=True)
+
+                try: # breaks if not utf-8, but oh well
+                    body = body.decode("utf-8")
+                except:
+                    continue
+                result.append({
+                    "sender": msg["From"],
+                    "subject": msg["Subject"],
+                    "body": body,
+                    "timestamp": timestamp
+                })
+
+    return result
+
+# adapted from http://naelshiab.com/tutorial-send-email-python/
+def send(outbox, sender, receiver, message):
+    outbox.sendmail(sender, receiver, ".\r\n" + message)
+
+def force_print(s):
+    sys.stdout.write(s)
+    sys.stdout.flush()
+
+WELCOME_MESSAGE = "Welcome to tsh.\r\n- reply to this message with shell commands\r\n- \"exit\" to exit"
+address = input("Email: ")
+password = getpass("Password: ")
+number = input("Phone number (numbers only): ")
+gateways = {
+    "Alltel": "message.alltel.com",
+    "AT&T": "txt.att.net",
+    "T-Mobile": "tmomail.net",
+    "Virgin Mobile": "vmobl.com",
+    "Sprint": "messaging.sprintpcs.com",
+    "Verizon": "vtext.com",
+    "Nextel": "messaging.nextel.com",
+    "US Cellular": "mms.uscc.net"
+}
+while True:
+    service = input("Phone service: ")
+    if service not in gateways:
+        print("Error: unknown service \"%s\"" % service)
+        print("Choose one of (%s)" % ", ".join([s for s in gateways]))
+    else:
+        gateway = gateways[service]
+        break
+phoneaddress = number + "@" + gateway
+
+force_print("Connecting... ")
+inbox = imaplib.IMAP4_SSL("imap.gmail.com")
+inbox.login(address, password)
+force_print("done.\n")
+
+# adapted from https://stackoverflow.com/questions/17332384/python-3-send-email-smtp-gmail-error-smtpexception
+force_print("Sending initial text... ")
+outbox = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+outbox.login(address, password)
+send(outbox, address, phoneaddress, WELCOME_MESSAGE)
+force_print("done.\n")
+
+last_retrieval = time.time()
+while True:
+    force_print("Retrieving requests... ")
+    new_retrieval = time.time()
+    requests = emails(inbox, phoneaddress, last_retrieval)
+    last_retrieval = new_retrieval
+    requests = [r["body"] for r in requests]
+    requests = [r[0:r.find("\r\n")] for r in requests]
+    requests = requests[::-1]
+    if len(requests) == 0:
+        force_print("got nothing.\n")
+    else:
+        force_print("got: %s\n" % "".join(["\n\t" + r for r in requests]))
+    for request in requests:
+        force_print("Processing request: %s\n" % request)
+        if request == "exit":
+            force_print("Exiting.\n")
+            send(outbox, address, phoneaddress, "Exiting tsh.")
+            outbox.quit()
+            exit()
+        result = subprocess.Popen(request, shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
+        force_print("Output: %s\n" % result)
+        force_print("Sending output to %s... " % phoneaddress)
+        send(outbox, address, phoneaddress, result)
+        force_print("done.\n")
